bbff7c7cb09283ffede480a009f892d69f1decd2
jacobmoneda <104470514+jacobmoneda@users.noreply.github.com>
2026-09-02 12:47:28 +1200

    add camera state

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,12 @@ import time
 import uvicorn
 from backend.camera.go2_camera import Go2Camera
 
+from unitree_sdk2py.core.channel import ChannelFactoryInitialize
+ChannelFactoryInitialize(0, "eth0")
+
 from backend.decision_logic import run_task
 from backend.utils.input_processor import process_input, InvalidPromptError
 
-# CHOOSE A MODEL TO USE FOR VISION-LANGUAGE PERCEPTION
-# Uncomment one of the following lines to select the model
-#from backend.vlm.qwen_engine import run_qwen_with_frame
-#from backend.vlm.smolvlm_engine import run_smolvlm_with_frame
 from backend.vlm.phi_engine import run_phi_with_frame
 
 
@@ -26,6 +25,7 @@ def perception_loop(camera: Go2Camera):
 
         t0 = time.time()
 
+        prompt = shared_state.user_prompt
 
         if not prompt:
             time.sleep(0.1)
@@ -54,6 +54,7 @@ def main():
     # Start camera
     camera = Go2Camera(network_interface="eth0")
     camera.start()
+    shared_state.camera = camera
 
     # Start perception loop in background thread
     perception_thread = threading.Thread(
